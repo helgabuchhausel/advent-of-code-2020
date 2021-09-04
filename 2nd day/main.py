85bd4c6928802a5978c7688402b7fa6077927988
Helga Eszter Buchhausel <48885428+helgabuchhausel@users.noreply.github.com>
2021-09-04 13:52:26 +0200

    2nd day 2nd part

--- 2nd day/main.py
+++ 2nd day/main.py
@@ -16,10 +16,28 @@ def partOne():
         letter = parts[1][:-1]
         password = parts[2]
 
+        min =int(constraints[0])
+        max = int(constraints[1])
         found = len(re.findall(letter, password))
 
-        if found >= int(constraints[0]) and found <= int(constraints[1]):
+        if found >= min and found <= max:
             i=i+1
     return i
 
+i = 0
+for line in lines:
+    parts = re.split(split, line)
+
+    constraints = re.split(split2, parts[0])
+    letter = parts[1][:-1]
+    password = parts[2]
+    min =int(constraints[0])
+    max = int(constraints[1])
+
+    if password[min-1] == letter or password[max-1] == letter :
+        if not(password[min-1] == letter and password[max-1] == letter)  :
+            i=i+1
+
+
 print(partOne())
+print(i)
