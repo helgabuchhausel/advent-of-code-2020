cea05dd9df10f0fb14ddba79458b19febac1a5e8
Helga Eszter Buchhausel <48885428+helgabuchhausel@users.noreply.github.com>
2021-09-04 14:14:50 +0200

    oop

--- 2nd day/main.py
+++ 2nd day/main.py
@@ -7,37 +7,53 @@ file = open('data.txt', 'r')
 lines = file.read().splitlines()
 file.close()
 
+class Object:
+    def __init__(self, line):
+        self.line = line
+        self.parts = re.split(split, line)
+        self.constraints = re.split(split2, self.parts[0])
+        self.letter = self.parts[1][:-1]
+        self.password = self.parts[2]
+        self.min =int(self.constraints[0])
+        self.max = int(self.constraints[1])
+        self.found = 0
+    
+    def find(self):
+        return len(re.findall(self.letter,  self.password))
+
+    def checkCorporatePassword(self):
+        if self.find() >= self.min and self.find() <= self.max:
+            return true
+    
+    def createIndexes(self):
+        self.index1 = self.min -1 
+        self.index2 = self.max -1 
+
+    def checkPassword(self):
+        self.createIndexes()
+        if self.password[self.index1] == self.letter or self.password[self.index2] == self.letter :
+            if not(self.password[self.index1] == self.letter and self.password[self.index2] == self.letter)  :
+                return true
+    
+    
 def partOne():
     i = 0
     for line in lines:
-        parts = re.split(split, line)
+        currentLine = Object(line)
 
-        constraints = re.split(split2, parts[0])
-        letter = parts[1][:-1]
-        password = parts[2]
-
-        min =int(constraints[0])
-        max = int(constraints[1])
-        found = len(re.findall(letter, password))
-
-        if found >= min and found <= max:
+        if currentLine.checkCorporatePassword():
             i=i+1
     return i
 
-i = 0
-for line in lines:
-    parts = re.split(split, line)
 
-    constraints = re.split(split2, parts[0])
-    letter = parts[1][:-1]
-    password = parts[2]
-    min =int(constraints[0])
-    max = int(constraints[1])
+def partTwo():
+    i = 0
+    for line in lines:
+        currentLine = Object(line)
 
-    if password[min-1] == letter or password[max-1] == letter :
-        if not(password[min-1] == letter and password[max-1] == letter)  :
+        if currentLine.checkPassword():
             i=i+1
-
+    return(i)
 
 print(partOne())
-print(i)
+print(partTwo())
