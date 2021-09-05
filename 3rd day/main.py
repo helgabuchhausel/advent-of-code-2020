32c67d0d9567b2f9ad21386d6797c11f0de31e04
Helga Eszter Buchhausel <48885428+helgabuchhausel@users.noreply.github.com>
2021-09-05 14:16:34 +0200

    3rd day

---
+++ 3rd day/main.py
@@ -0,0 +1,27 @@
+with open("data.txt", "r") as file:
+    map = list(map(str.strip, file.readlines()))
+    w = len(map[0])
+file.close()
+
+tree = "#"
+slopes = [(1,1), (3,1), (5,1), (7,1), (1, 2)]
+
+def countTrees(slopeX, slopeY):
+    x, trees = 0, 0
+    for y in range(0, len(map), slopeY):
+        if map[y][x] == tree:
+            trees += 1
+        x = (x + slopeX) % w
+    return trees
+
+def productOfSlopes():
+    product = 0
+    for slope in slopes:
+        if product == 0:
+            product = countTrees(slope[0],slope[1])
+        else:
+            product *= countTrees(slope[0],slope[1])
+    return product
+
+print(countTrees(3, 1))
+print(productOfSlopes())
