a915d25682a09127444a5832607fbd0fe7dcaa25
Helga Eszter Buchhausel <48885428+helgabuchhausel@users.noreply.github.com>
2021-09-08 16:40:27 +0200

    day 5 part 2

--- 5th day/main.py
+++ 5th day/main.py
@@ -1,19 +1,43 @@
+from logging import BufferingFormatter
+
+
 file = open('data.txt', 'r')
 lines = file.read().splitlines()
 file.close()
 
-binaryPasses = []
-#converts the boarding passes to binary
-for boardingpass in lines:
-    binarypass = boardingpass.replace("F", "0").replace("B", "1").replace("R", "1").replace("L", "0") # replaced to look like binary 
-    binaryPasses.append(binarypass)
+def convertToBin():
+    binaryPasses = []
+    #converts the boarding passes to binary
+    for boardingpass in lines:
+        binarypass = boardingpass.replace("F", "0").replace("B", "1").replace("R", "1").replace("L", "0")
+        binaryPasses.append(binarypass)
+    return binaryPasses
 
-#sorting the values 
-sortedBin = sorted(binaryPasses)
+# the highest seat ID on a boarding pass
+def highestPass():
+    #sorting binary values 
+    sortedBin = sorted(convertToBin())
+    #returns the highest value in decimal
+    return int(sortedBin[-1], 2)
 
-#returns the highest value 
-max = sortedBin[-1]
+#ID of your seat
+def partTwo():
+    #sorting the values 
+    sortedBin = sorted(convertToBin())
+    max= int(sortedBin[-1], 2)
+    mySeat = 0
+    #loops through all seats from the list
+    for x in range(max):
+        if x < len(sortedBin):
+            temp1 = int(sortedBin[x], 2) # to dec
+            temp2 = int(sortedBin[x+1], 2)
+            #compares the two seat numbers
+            if temp2-1 != temp1:
+                mySeat = temp1+1
+                break
+        else:
+            break
+    return(mySeat)
 
-a = int(max, 2) # converts it to decimal value
-print(a)
-
+print(highestPass())
+print(partTwo())
